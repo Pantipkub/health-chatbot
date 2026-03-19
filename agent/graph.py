14084237324f75d24f6b5095dbefc9742c4b4be9
Piyapanchanok Meeboonsalang <piyapanchanok@gmail.com>
2026-03-20 01:33:17 +1100

    Replace personal API key with given JSON for Vertex

--- agent/graph.py
+++ agent/graph.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
 from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
 from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_vertexai import ChatVertexAI
 from langchain_core.tools import tool
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, END
@@ -33,12 +34,12 @@ tools = []
 
 # model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite").bind_tools(tools)
 
-intent_model = ChatGoogleGenerativeAI(
+intent_model = ChatVertexAI(
     model="gemini-2.5-flash-lite",
     temperature=0
 )
 
-chat_model = ChatGoogleGenerativeAI(
+chat_model = ChatVertexAI(
     model="gemini-2.5-flash"
 )
 
